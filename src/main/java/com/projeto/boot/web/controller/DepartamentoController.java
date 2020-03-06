3fffce52ca3f9f3372127d1a2af8d809ce5474f9
Rômulo Junges <romuloatj2@gmail.com>
2020-03-06 12:50:53 -0300

    Desenvolvido o CRUD para o Departamento

--- src/main/java/com/projeto/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/projeto/boot/web/controller/DepartamentoController.java
@@ -1,20 +1,64 @@
 package com.projeto.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.projeto.boot.domain.Departamento;
+import com.projeto.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
 		return "/departamento/lista"; 
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento, RedirectAttributes attr) {
+		service.salvar(departamento);
+		attr.addFlashAttribute("success", "Departamento inserido com sucessos.");
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "/departamento/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Departamento departamento, RedirectAttributes attr) {
+		service.editar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucessos.");
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		if(service.departamentoTemCargo(id)) {
+			model.addAttribute("fail", "Departamento não removido, possui cargo(s) vinculado(s).");
+		} else {
+			service.excluir(id);
+			model.addAttribute("success", "Departamento removido com sucesso");
+		}
+		
+		return listar(model);
 	}
 }
