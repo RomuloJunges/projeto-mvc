60a206059bdc6e4a599116c2002cac21118c760a
Rômulo Junges <romuloatj2@gmail.com>
2020-03-07 18:53:01 -0300

    Implementado o CRUD para os Funcionarios

--- src/main/java/com/projeto/boot/DemoMvcApplication.java
+++ src/main/java/com/projeto/boot/DemoMvcApplication.java
@@ -1,12 +1,21 @@
 package com.projeto.boot;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class DemoMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMvcApplication.class, args);
+	}
+
+	@Bean
+	public FixedLocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 }
